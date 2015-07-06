47d8246f955e5a19f99b8b35f3bb590dabdf882e
Kuszki <l.drozdz@o2.pl>
2015-07-06 22:18:29 +0200

    Add KLScript QT bindings

---
+++ qt/klscriptbinding.cpp
@@ -0,0 +1,85 @@
+/***********************************************************************
+ *
+ * QT bindings for KLLib KLScript
+ * Copyright (C) 2015  Łukasz "Kuszki" Dróżdż
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ **********************************************************************/
+
+#include "klscriptbinding.hpp"
+
+KLScriptbinding::KLScriptbinding(QObject *Parent)
+: QObject(Parent), KLScript() {}
+
+KLScriptbinding::~KLScriptbinding(void) {}
+
+void KLScriptbinding::SetCode(const QString& Script)
+{
+	LastCode = Script;
+}
+
+bool KLScriptbinding::Evaluate(void)
+{
+	const bool OK = KLScript::Evaluate(LastCode.toStdString().c_str());
+
+	LastLine = KLScript::GetLine(LastCode.toStdString().c_str());
+
+	if (OK) emit onEvaluate(LastReturn);
+
+	return OK;
+}
+
+bool KLScriptbinding::Validate(const QString& Script)
+{
+	const bool OK = KLScript::Validate(Script.toStdString().c_str());
+
+	LastLine = KLScript::GetLine(Script.toStdString().c_str());
+
+	return OK;
+}
+
+KLScriptbinding::ERROR KLScriptbinding::GetError(void) const
+{
+	return LastError;
+}
+
+QString KLScriptbinding::GetMessage(void) const
+{
+	switch (LastError)
+	{
+		case UNDEFINED_VARIABLE:		return tr("Encountered undefined variable");
+		case UNDEFINED_FUNCTION:		return tr("Encountered undefined function bind");
+		case EXPECTED_ENDIF_TOK:		return tr("Expected `fi` token");
+		case EXPECTED_DONE_TOK:		return tr("Expected `done` token");
+		case EXPECTED_TERMINATOR:	return tr("Expected `;` token");
+		case EMPTY_EXPRESSION:		return tr("Encountered empty expression");
+		case UNKNOWN_EXPRESSION:		return tr("Encountered unknown expression");
+		case WRONG_SCRIPTCODE:		return tr("Wrong or empty Scriptcode");
+		case WRONG_PARAMETERS:		return tr("Encountered invalid expresion parameters");
+		case WRONG_EVALUATION:		return tr("Encountered invalid math expresion");
+
+		default: return tr("Script is valid");
+	}
+}
+
+int KLScriptbinding::GetLine(void) const
+{
+	return LastLine;
+}
+
+double KLScriptbinding::GetValue(void) const
+{
+	return LastReturn;
+}
---
+++ qt/klscriptbinding.hpp
@@ -0,0 +1,136 @@
+/***********************************************************************
+ *
+ * QT bindings for KLLib KLScript
+ * Copyright (C) 2015  Łukasz "Kuszki" Dróżdż
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ **********************************************************************/
+
+#ifndef KLSCRIPTBINDING_HPP
+#define KLSCRIPTBINDING_HPP
+
+#include "../libbuild.hpp"
+
+#include "../script/klscript.hpp"
+
+#include <QObject>
+
+class KLScriptbinding : public QObject, KLScript
+{
+
+		Q_OBJECT
+
+	protected:
+
+		QString LastCode;	//!< Ostatni przetwarzany skrypt.
+
+		int LastLine;		//!< Ostatnia przetwarzana linia.
+
+	public:
+
+		using KLScript::Variables;
+		using KLScript::Bindings;
+		using KLScript::Parser;
+
+		/*! \brief		Konstruktor.
+		 *  \param [in]	Parent Rodzic obiektu.
+		 *
+		 * Buduje obiekt zgodnie z mechanizmem `QObject`.
+		 *
+		 */
+		explicit KLScriptbinding(QObject* Parent = nullptr);
+
+		/*! \brief		Destruktor.
+		 *
+		 * Niszczy obiekt zgodnie z mechanizmem `QObject`.
+		 *
+		 */
+		virtual ~KLScriptbinding(void) override;
+
+		/*! \brief		Zapisanie skryptu.
+		 *  \param [in]	Script Skrypt do zapisania.
+		 *
+		 * Zapamiętuje skrypt w celu późniejszego wywołania.
+		 *
+		 */
+		void SetCode(const QString& Script);
+
+		/*! \brief		Wywołanie skryptu.
+		 *  \return 		Powodzenie operacji.
+		 *  \see			LastError(), LastValue().
+		 *
+		 * Przetwarza podane wyrażenie i zwraca powodzenie jego wykonania.
+		 *
+		 */
+		bool Evaluate(void);
+
+		/*! \brief		Sprawdzenie skryptu.
+		 *  \param [in]	Script Skrypt do sprawdzenia.
+		 *  \return 		Powodzenie operacji.
+		 *
+		 * Przetwarza podany skrypt i sprawdza jego poprawność.
+		 *
+		 */
+		bool Validate(const QString& Script);
+
+		/*! \brief		Pobranie wartości.
+		 *  \return		Ostatnia poprawnie obliczona wartość.
+		 *  \see			Evaluate().
+		 *
+		 * Pobiera ostatnią obliczoną wartość.
+		 *
+		 */
+		ERROR GetError(void) const;
+
+		/*! \brief		Pobranie opisu ostatniego błędu.
+		 *  \return		Opis ostatniego błędu.
+		 *  \see			GetError().
+		 *
+		 * Zwraca czytelny dla człowieka opis napotkanego błędu.
+		 *
+		 */
+		QString GetMessage(void) const;
+
+		/*! \brief		Pobranie numeru linii.
+		 *  \return		Ostatnio przetwarzana linia.
+		 *
+		 * Pobiera ostatnio przetwarzaną linie.
+		 *
+		 */
+		int GetLine(void) const;
+
+		/*! \brief		Pobranie wartości.
+		 *  \return		Ostatnia poprawnie obliczona wartość.
+		 *  \see			Evaluate().
+		 *
+		 * Pobiera ostatnią obliczoną wartość.
+		 *
+		 */
+		double GetValue(void) const;
+
+	signals:
+
+		/*! \brief		Zdarzenie obliczenia wartości.
+		 *  \param [in]	Value Wartość wyrażenia.
+		 *  \see			Evaluate().
+		 *
+		 * Zdarzenie wywoływane przy poprawnym obliczeniu wartości wyrażenia.
+		 *
+		 */
+		void onEvaluate(double);
+
+};
+
+#endif // KLSCRIPTBINDING_HPP
