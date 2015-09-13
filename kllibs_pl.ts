370e98e4306a7962b4b9e57da3d31edbb568e225
Kuszki <l.drozdz@o2.pl>
2015-09-13 22:50:23 +0200

    Add translations

---
+++ kllibs_pl.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>KLParserbinding</name>
+    <message>
+        <location filename="qt/klparserbinding.cpp" line="51"/>
+        <source>Encountered unexpected operator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klparserbinding.cpp" line="52"/>
+        <source>Encountered unknown operator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klparserbinding.cpp" line="53"/>
+        <source>Encountered unknown function</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klparserbinding.cpp" line="54"/>
+        <source>Expected bracket</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klparserbinding.cpp" line="55"/>
+        <source>Expected function paramters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klparserbinding.cpp" line="56"/>
+        <source>Expected operator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klparserbinding.cpp" line="57"/>
+        <source>Encountered wrong bracket number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klparserbinding.cpp" line="58"/>
+        <source>Encountered division by zero</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klparserbinding.cpp" line="60"/>
+        <source>Script is valid</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KLScriptbinding</name>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="75"/>
+        <source>Encountered undefined variable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="76"/>
+        <source>Encountered undefined function bind</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="77"/>
+        <source>Expected `fi` token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="78"/>
+        <source>Expected `done` token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="79"/>
+        <source>Expected `;` token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="80"/>
+        <source>Encountered empty expression</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="81"/>
+        <source>Encountered unknown expression</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="82"/>
+        <source>Wrong or empty Scriptcode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="83"/>
+        <source>Encountered invalid expresion parameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="84"/>
+        <source>Encountered invalid math expresion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/klscriptbinding.cpp" line="86"/>
+        <source>Script is valid</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
