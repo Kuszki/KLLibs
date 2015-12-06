3bbd9c01356fbf694a246701672d4b2a8298ec91
Kuszki <l.drozdz@o2.pl>
2015-12-06 13:20:57 +0100

    Update docs

--- script/klscript.hpp
+++ script/klscript.hpp
@@ -200,7 +200,8 @@ class KLLIBS_EXPORT KLScript
 		bool Evaluate(const KLString& Script, KLList<double>* Params = nullptr);
 
 		/*! \brief		Sprawdzenie kodu.
-		 *  \param [in]	Script Skrypt do przetworzenia.
+		 *  \param [in]	Script	Skrypt do przetworzenia.
+		 *  \param [in]	Params	Globalny zakres zmiennych.
 		 *  \return		Powodzenie operacji.
 		 *
 		 * Przetwarza wybrany kod pod kątem błędów składni i zwraca powodzenie operacji.
